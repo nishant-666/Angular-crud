a580ed19e3c3e97a639221d98f7102f6f2fc45b3
Nishant kumar <nishants440@gmail.com>
2022-03-12 14:13:13 +0530

    Starter Code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { UsersService } from './users.service';
 
 @Component({
   selector: 'app-root',
@@ -8,43 +7,8 @@ import { UsersService } from './users.service';
 })
 export class AppComponent {
   title = 'angular-crud';
-  constructor(
-    private userService: UsersService
-  ) { }
 
-  ngOnInit() {
-    this.userService.getAllData()
-      .subscribe(data => {
-        console.log(data)
-      })
-  }
   submitData(value: any) {
-    let body = {
-      name: value.name,
-      age: value.age
-    }
-    this.userService.postData(body)
-    .subscribe(response => {
-      console.log(response)
-    })
-  }
-
-  updateData(value: any){
-    let body = {
-      name: value.name,
-      age: value.age
-    }
-
-    this.userService.updateData(body, `622c55fbdea0f4aaa5cdfc4f`)
-    .subscribe(response => {
-      console.log(response);
-    })
-  }
-
-  delete(){
-    this.userService.deleteData(`622c573cf23ce54e445b2bed`)
-    .subscribe(response => {
-      console.log(response);
-    })
+    console.log(value)
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -13,7 +12,6 @@ import { HttpClientModule } from '@angular/common/http';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
